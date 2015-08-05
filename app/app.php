bf9ea659ac700d30f85b92eae4b68d8fe04aae69
Ashlin Aronin & Rick Hills <student@epicodus.com>
2015-08-05 15:32:41 -0700

    Display all job data on resume page

--- app/app.php
+++ app/app.php
@@ -13,24 +13,25 @@
         'twig.path' => __DIR__.'/../views'
     ));
 
-    $app->get("/", function() use ($app) {
-        return $app['twig']->render('resume.html.twig');
 
+    // We don't need another route because display is dependent on array content
+    $app->get("/", function() use ($app) {
+            return $app['twig']->render('resume.html.twig', array('resume' => Job::getAll()));
     });
 
-    $app->post("/create_job", function() use ($app) {
+    $app->get("/create_job", function() use ($app) {
+        return $app['twig']->render('create_job.html.twig');
+    });
+
+    $app->post("/job_created", function() use ($app) {
         $job = new Job($_POST['company'],$_POST['title'],$_POST['start_date'],$_POST['end_date'],$_POST['address'],$_POST['salary']);
         $job->save();
-        return $app['twig']->render('create_job.html.twig', array('newjob' => $job));
+        return $app['twig']->render('job_created.html.twig', array('newjob' => $job));
     });
+
+    // make delete_job route
+
 
     return $app;
 
 ?>
-
-private $company;
-private $title;
-private $start_date;
-private $end_date;
-private $address;
-private $salary;
